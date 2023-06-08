1c5cda9dd8c0b92121ea4c001be11a07a6542feb
NeilBouiche <neil.bouiche@icloud.com>
2023-06-08 11:39:44 +0200

    Final

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -16,6 +16,9 @@ export default function Heading() {
 
 const UserNameContainer = styled.div`
   margin: 12rem 0 0 14rem;
+  @media (min-width: 1440px) {
+    margin: 12rem 10% 0 20rem
+  }
 `;
 
 const HelloName = styled.h1`
