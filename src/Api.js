556db1ab5f556a6b261f682249a7d3178191c310
Neil Bouiche <neil.bouiche@icloud.com>
2023-06-21 11:22:27 +0200

    Final Error 500

--- src/Api.js
+++ src/Api.js
@@ -2,42 +2,35 @@ import { useState, useEffect } from "react";
 import "../src/mock/mock";
 import { isApi } from "./conf";
 
-export function fetchData(userId, additionalParam) {
+export async function fetchData(userId, additionalParam) {
   if (!isApi) {
-    return import("./mock/mock").then((data) => data.default);
+    const data = await import("./mock/mock");
+    return data.default;
   } else {
-    let url = `http://localhost:3000/user/${userId}/${additionalParam}`;
-    return fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Error 500: Internal Server Error");
-        }
-        return res.json();
-      })
-      .then((data) => data);
+    try {
+      let url = `http://localhost:3000/user/${userId}/${additionalParam}`;
+      const res = await fetch(url);
+      if (res.status === 500) {
+        throw new Error("Internal Server Error");
+      }
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      document.getElementById("root").innerHTML =
+        "Error 500: Internal Server Error";
+      console.log("Error 500: Internal Server Error");
+    }
   }
 }
 
 export function useData({ additionalParam }) {
   const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetchData(12, additionalParam)
-      .then((data) => {
-        setUser(data);
-        setError(null);
-      })
-      .catch((error) => {
-        setUser(null);
-        setError(error.message);
-      });
+    fetchData(12, additionalParam).then((data) => {
+      setUser(data);
+    });
   }, [additionalParam]);
-
-  if (error) {
-    console.log(error);
-    return <div>{error}</div>;
-  }
 
   return user;
 }
