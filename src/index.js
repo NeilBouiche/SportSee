62c6c58c2aa61c0fe5c5d4d456ce2cbb6e4f8e8a
Neil Bouiche <neil.bouiche@icloud.com>
2024-12-16 21:37:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Selector />} />
         <Route path="/user/:userId" element={<Home />} />
         <Route path="*" element={<Error />} />
       </Routes>
